ffe04f140d8383fea8218f93b6bc7cb14aec7588
Methawee A <merimo.mayjiiex@hotmail.com>
2017-05-02 23:09:44 +0700

    add searchview to menu bar

--- app/src/main/java/com/example/methawee/myapplication/main/BookActivity.java
+++ app/src/main/java/com/example/methawee/myapplication/main/BookActivity.java
@@ -1,10 +1,16 @@
 package com.example.methawee.myapplication.main;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 
 
@@ -18,12 +24,10 @@ import java.util.ArrayList;
 
 public class BookActivity extends AppCompatActivity implements BookView {
 
-    public static final String BOOK_DETAIL_KEY = "book";
     private ListView book_view;
     private BookPresenter book_presenter;
     private BookRepository book_repository;
     private BookAdapter book_adapter;
-
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
@@ -40,6 +44,29 @@ public class BookActivity extends AppCompatActivity implements BookView {
         book_view.setAdapter(book_adapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_view_menu_item, menu);
+        MenuItem searchViewItem = menu.findItem(R.id.action_search);
+        final SearchView searchViewAndroidActionBar = (SearchView) MenuItemCompat.getActionView(searchViewItem);
+        searchViewAndroidActionBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchViewAndroidActionBar.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        SearchView searchView = (SearchView) searchViewItem.getActionView();
+        searchView.setQueryHint("search your book here ♡");
+        return super.onCreateOptionsMenu(menu);
+    }
 }
 
 
